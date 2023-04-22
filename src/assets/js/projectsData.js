6ad3539099359cc497a58ba1cfb73ec615d39d98
Trappin Recidivism <austinslater58@gmail.com>
2023-04-22 01:11:06 -0500

    projects render accordingly

---
+++ src/assets/js/projectsData.js
@@ -0,0 +1,72 @@
+import bugInvaders from '../projectImgs/bugInvaders.png';
+import winterWonderland from '../projectImgs/winterWonderland.png';
+import cubeMatcher from '../projectImgs/cubeMatcher.png';
+import concertScout from '../projectImgs/concertScout.png';
+import travelBug from '../projectImgs/travelBug.png';
+import recipEase from '../projectImgs/recipEase.png';
+import jamming from '../projectImgs/jamming.png';
+import catWorx from '../projectImgs/catWorx.png';
+
+
+
+const projectsData = [
+    {
+        id: 1,
+        title: 'BugInvaders',
+        description: 'A Space Invaders-style arcade game built with Phaser.js',
+        image: bugInvaders,
+        link: 'https://austinslatey.github.io/bug-invaders/'
+    },
+    {
+        id: 2,
+        title: 'CubeMatcher',
+        description: 'A puzzle game built with Phaser.js',
+        image: cubeMatcher,
+        link: 'https://austinslatey.github.io/Cube-Matcher/'
+    },
+    {
+        id: 3,
+        title: 'WinterWonderland',
+        description: 'A holiday-themed platformer built with Phaser.js',
+        image: winterWonderland,
+        link: 'https://austinslatey.github.io/winter-wonderland/'
+    },
+    {
+        id: 4,
+        title: 'ConcertScout',
+        description: 'A concert and music festival discovery app built with HTML, CSS, JavaScript, & Third Party APIs',
+        image: concertScout,
+        link: 'https://austinslatey.github.io/concert-scout/'
+    },
+    {
+        id: 5,
+        title: 'TravelBug',
+        description: 'A travel journal app built with React, Node.js, and MongoDB',
+        image: travelBug,
+        link: 'https://github.com/Metelak/Travel-bug'
+    },
+    {
+        id: 6,
+        title: 'RecipEase',
+        description: 'A recipe sharing app built with React, Node.js, and PostgreSQL',
+        image: recipEase,
+        link: 'https://github.com/austinslatey/RecipEase'
+    },
+    {
+        id: 7,
+        title: 'Jamming',
+        description: 'A music discovery app built with React and the Spotify API',
+        image: jamming,
+        link: 'https://bucolic-baklava-e825fb.netlify.app'
+    },
+    {
+        id: 8,
+        title: 'CatWorx',
+        description: 'A cat-themed security company website built with C#',
+        image: catWorx,
+        link: 'https://github.com/austinslatey/Catwork-Security'
+    },
+
+];
+
+export default projectsData;
--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>Content</h1>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold mb-8">Projects</h1>
+    <div class="flex flex-wrap -mx-4">
+      <div v-for="project in projectsData" :key="project.id" class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+        <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+          <a :href="project.link" target="_blank">
+            <img :src="project.image" alt="project" class="h-64 w-full object-cover" />
+          </a>
+          <div class="p-4">
+            <h2 class="text-xl font-semibold mb-2">{{ project.title }}</h2>
+            <p class="text-gray-700">{{ project.description }}</p>
+            <a :href="project.link" target="_blank" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full inline-block">View {{ project.title }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import projectsData from '@/assets/js/projectsData.js';
 export default {
-  name: "Projects",
-};
+  name: 'Projects',
+  data() {
+    return {
+      projectsData: projectsData
+    }
+  }
+}
 </script>
+
+<style>
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
