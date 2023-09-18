0c6b6fdc0555d937b1ae490c38cb967729ed6c70
Austin Slater <austinslater58@gmail.com>
2023-09-18 14:56:39 -0500

    fixed the overlapping of skills text on mobile-viewport

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,15 @@
 <template>
-  <h2 class="mb-4 text-3xl text-center text-white">Skills:</h2>
-  <div class="mt-10 min-h-screen">
-    
-    <div class="grid grid-cols-3 gap-4 p-2">
-      <div v-for="(skill, index) in skills" :key="index" class="mb-2 m-2 p-2 text-center bg-white rounded-3xl">
-          <a :href="skill.url" class="text-primary-color hover:text-secondary-color text-xl">
+  <div>
+    <h2 class="mb-4 text-3xl text-center text-white">Skills:</h2>
+    <div class="mt-10 min-h-screen">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2">
+        <div v-for="(skill, index) in skills" :key="index" class="mb-2 m-2 p-2 text-center bg-white rounded-3xl">
+          <a :href="skill.url" class="text-primary-color hover:text-secondary-color text-lg sm:text-xl">
             <img :src="skill.icon" alt="Skill Icon" class="w-10 h-10 mx-auto mb-2">
             {{ skill.name }}
           </a>
         </div>
+      </div>
     </div>
   </div>
 </template>
