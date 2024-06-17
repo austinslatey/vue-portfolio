121f025c50aeeae56e25dcc2233d332df8a56c83
Austin Slater <austinslater58@gmail.com>
2024-06-16 23:36:47 -0500

    added more certificates to page

--- src/assets/js/certificateData.js
+++ src/assets/js/certificateData.js
@@ -6,57 +6,84 @@ import html from '../imgs/certificateImg/html.png';
 import command from '../imgs/certificateImg/command.png';
 import css from '../imgs/certificateImg/css.png';
 import css2 from '../imgs/certificateImg/cc2.png';
-import harvard from '../imgs/certificateImg/harvard.png';
+import cs50X from '../imgs/certificateImg/harvard.png';
+import cs50P from '../imgs/certificateImg/python.jpg';
+import cs50Ai from '../imgs/certificateImg/ai.jpg';
+import it from '../imgs/certificateImg/it.png';
+import linux from '../imgs/certificateImg/linux.png';
 
 // image data to render on certificates page
 
 const certificateData = [
-    {
-      id: 1,
-      image: UofM,
-      link: 'https://www.credly.com/badges/6edfd852-b6d0-46a9-9c33-33c13f2df408'
-    },
-    {
-      id: 2,
-      image: harvard,
-      link: 'https://cs50.harvard.edu/certificates/4da06f51-6c5b-4fa0-8787-14d8c51ca7ec'
+  {
+    id: 1,
+    image: UofM,
+    link: 'https://www.credly.com/badges/6edfd852-b6d0-46a9-9c33-33c13f2df408'
+  },
+  {
+    id: 2,
+    image: cs50X,
+    link: 'https://cs50.harvard.edu/certificates/4da06f51-6c5b-4fa0-8787-14d8c51ca7ec'
 
-    },
-    {
-      id: 3,
-      image: reactJs,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5c9ce0b45f1de879ebcad4fd'
-    },
-    {
-      id: 4,
-      image: phaserJs,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5ddbff3dd1bd3b001397ae9f'
-    },
-    {
-      id: 5,
-      image: javaScript,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/705dcb15de0da4dd9d9fc4f3274b430e'
-    },
-    {
-      id: 6,
-      image: html,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5cadfefe5f1de806e9704577'
-    },
-    {
-      id: 7,
-      image: command,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/c87ba0541f8be78bc2f4ba1128233f6f'
-    },
-    {
-      id: 8,
-      image: css,
-      link: 'https://www.codecademy.com/profiles/austinslatey/certificates/3a62023b0054dc793edc0adecd715fd7'
-    },
-    {
-        id: 9,
-        image: css2,
-        link: 'https://www.codecademy.com/profiles/austinslatey/certificates/9a5bb1fc45b4281af1fffec93b0aaf05'
-      },
-  ];
+  },
+  {
+    id: 3,
+    image: cs50P,
+    link: 'https://certificates.cs50.io/143eb274-8ec9-49d8-8683-6015a6aeb66f'
+
+  },
+  {
+    id: 4,
+    image: cs50Ai,
+    link: 'https://certificates.cs50.io/dc19cc46-5c18-477c-ab4a-95e7f7c06f2b'
+
+  },
+  {
+    id: 5,
+    image: reactJs,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5c9ce0b45f1de879ebcad4fd'
+  },
+  {
+    id: 6,
+    image: phaserJs,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5ddbff3dd1bd3b001397ae9f'
+  },
+  {
+    id: 7,
+    image: javaScript,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/705dcb15de0da4dd9d9fc4f3274b430e'
+  },
+  {
+    id: 8,
+    image: html,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/5cadfefe5f1de806e9704577'
+  },
+  {
+    id: 9,
+    image: linux,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/54de574543f242baaf263227f82073bf'
+  },
+  {
+    id: 10,
+    image: it,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/f8fcb12eaa0c4dddaa2ebc8cfb95b8e7'
+  },
   
+  {
+    id: 11,
+    image: command,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/c87ba0541f8be78bc2f4ba1128233f6f'
+  },
+  {
+    id: 12,
+    image: css,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/3a62023b0054dc793edc0adecd715fd7'
+  },
+  {
+    id: 13,
+    image: css2,
+    link: 'https://www.codecademy.com/profiles/austinslatey/certificates/9a5bb1fc45b4281af1fffec93b0aaf05'
+  },
+];
+
 export default certificateData;
